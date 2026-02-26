7e44608626cf26fac175e7e4cc2951aab3df7f3f
skytheboundry <skytheboundry@gmail.com>
2026-02-26 23:12:13 +0530

    mobile header fixed

--- my-app/components/navbar.tsx
+++ my-app/components/navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
 
   return (
     <nav className="bg-white/95 backdrop-blur-md shadow-sm border-b fixed w-full z-50">
-      <div className="max-w-full ml-20 mr-20 mx-auto px-6 py-3 flex justify-between items-center">
+      <div className="max-w-7xl mx-auto px-6 py-3 flex justify-between items-center">
 
         {/* Logo */}
         <Link href="/" className="flex items-center">
@@ -43,7 +43,7 @@ export default function Navbar() {
 
       {/* Mobile Menu */}
       {open && (
-        <div className="md:hidden bg-white shadow-lg">
+        <div className="md:hidden font-medium text-gray-700">
           <div className="flex flex-col items-center py-4 gap-4 font-medium">
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
